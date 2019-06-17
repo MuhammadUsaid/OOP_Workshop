1a4e59e0f13d4ce6e2eb977c5fe6fb2f27ef2f90
Muhammad Usaid <mohammadusaid40@gmail.com>
2019-06-17 12:11:30 +0500

    Day 6 resources added

---
+++ Revision_Arrays_Enums.cpp
@@ -0,0 +1,195 @@
+//1
+
+
+
+
+//#include <iostream>
+//int main()
+//{
+//    std::cout << "Hello";
+//    return 0;
+//}
+
+
+
+
+//2
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    cout << "Hello";
+//    return 0;
+//}
+
+//3
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    cout << "Hello";
+//}
+
+//4
+//#include <iostream>
+//using namespace std;
+//
+//int add(int, int);
+//int main()
+//{
+//    cout << add(2,4);
+//    return 0;
+//}
+//int add(int a, int b)
+//{
+//    return a + b;
+//}
+
+
+//5
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    int x = 7;
+//    bool a = x == 3;
+//    return 0;
+//}
+
+//6
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    int a = 6;
+//    if(a = 6)
+//        cout << 6;
+//    return 0;
+//}
+
+
+//7
+// Ternary Statement
+//switch cases
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    int x = 7;
+//    int a = x > 7 ? 10 : 20;
+//    switch(x)
+//    {
+//    case 7:
+//        cout << "7";
+//        break;
+//    case 8:
+//        cout << "8";
+//    }
+//    return 0;
+//}
+
+
+//8
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    double i;
+//    for(i = 0; i < 5; i++)
+//        cout << i;
+//    return 0;
+//}
+
+
+//9
+//Loops
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    int i = 0;
+//    while (i < 5)
+//    {
+//        cout << i << '\t';
+//        ++i;
+//    }
+//    for ( ; ; )
+//    {
+//
+//    }
+//    return 0;
+//}
+
+//Overloading
+
+
+//#include <iostream>
+//using namespace std;
+//int add(int a, int b)
+//{
+//    return a + b;
+//}
+//int add(int a, int b, int c)
+//{
+//    return a + b + c;
+//}
+//enum COLOR
+//{
+//    PINK,
+//    BLUE,
+//    BLACK
+//};
+//enum PARSERESULT
+//{
+//    SUCCESS = 0,
+//    ERROR_READINGFILE = -2,
+//    ERROR_OPENFILE = -1,
+//    ERROR_PARSEFILE = -3,
+//};
+//PARSERESULT readFileContents()
+//{
+//    if (!openFile())
+//        return ERROR_OPENFILE;
+//    if (!readFile())
+//        return ERROR_READINGFILE;
+//    if (!parseFile())
+//        return ERROR_PARSEFILE;
+//
+//    return 0; // success
+//}
+//
+//int main()
+//{
+//    COLOR c;
+//    //int c = PINK;
+//    //std::cout << c;
+//    cin >> c;
+//    return 0;
+//}
+
+
+//Arrays
+//int main()
+//{
+//    int b[3];
+//    b[0] = 1;
+//    b[1] = 2;
+//    b[2] = 3;
+//    b[3] = 4;
+//
+//    std::cout << b[3] << std::endl;
+//    int a[5] = {0, 1, 2};
+//
+//    int array[4]{};
+//    cout << a[3] << ' ' << a[5] ;
+//    return 0;
+//}
